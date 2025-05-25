352ccfcf30e43f2c2848114427a651295bd87381
deepshikhabatra3@gmail.com <deepshikhabatra3@gmail.com>
2025-05-25 17:15:48 +0530

    commit main code

--- BaseApplication/BaseApplication/Models/ErrorViewModel.cs
+++ BaseApplication/BaseApplication/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace BaseApplication.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; } = "test again revert";
+        public string? RequestId { get; set; } = "test main merge branch yesy";
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
