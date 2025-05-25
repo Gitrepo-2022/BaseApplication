2608d6e448f02479e1dbea8c963868a06f87455b
deepshikhabatra3@gmail.com <deepshikhabatra3@gmail.com>
2025-05-25 16:30:46 +0530

    changes update

--- BaseApplication/BaseApplication/Models/ErrorViewModel.cs
+++ BaseApplication/BaseApplication/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace BaseApplication.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? RequestId { get; set; } = "test again revert";
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
