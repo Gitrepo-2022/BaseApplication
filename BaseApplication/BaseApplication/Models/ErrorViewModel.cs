6b0461334cd2c7021bf248de6257ac8a946610e3
deepshikhabatra3@gmail.com <deepshikhabatra3@gmail.com>
2025-05-25 16:22:50 +0530

    commit changes for revert test

--- BaseApplication/BaseApplication/Models/ErrorViewModel.cs
+++ BaseApplication/BaseApplication/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace BaseApplication.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? RequestId { get; set; } = "test revert file";
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
